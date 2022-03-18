d63ddabd4e5e46752c5c7f7d2a245f49bc6bc78b
andrewmoquia <andrewmoquia09@gmail.com>
2022-03-18 13:09:28 +0800

    fixed: simplified.

--- src/model/user.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const usersSchema = new Schema({
-  username: { type: String }  
-});
-
-const User = model("users_freecodecamp", usersSchema);
-
-module.exports = User;
